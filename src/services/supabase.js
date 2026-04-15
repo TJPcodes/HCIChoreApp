a314c37636acfa160a0d780d49f78b827408e3dd
WilsonGoins <wilson.goins@ufl.edu>
2026-04-15 17:38:46 -0400

    hooking up to supabase for central db

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://hmrgohgbrpicibmqyckn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtcmdvaGdicnBpY2libXF5Y2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyODAxMjksImV4cCI6MjA5MTg1NjEyOX0.FyksT5AsPTmtDlJFYcW8k8o3ldLBKu7XwmcJu5Z5_aM';  
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
